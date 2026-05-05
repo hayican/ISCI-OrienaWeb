47775bba99e1e465e9a22a107bfc1d545fac3f95
Braja Khaslana <aryachakabrajawisesa@gmail.com>
2026-05-05 07:57:29 +0700

    penataan folder

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light"], // Kita kunci di tema terang dulu biar vibe bakery-nya dapet
+  },
+}
